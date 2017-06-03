da0200e23997d1a9cebc8f47c53b53373c4fe343
Dmitry <dmitry-orlov@hotmail.com>
2017-06-03 14:34:48 +0200

    not much changes

---
+++ BushWhack/app/src/main/java/bw/bushwhack/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package bw.bushwhack.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bw.bushwhack.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
